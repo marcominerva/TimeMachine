e7b8d77053200731b76f48f266a34bb493b9576f
Marco Minerva <marco.minerva@gmail.com>
2023-06-13 18:03:39 +0200

    Add MinimalHelpers.OpenApi

--- TimeMachine/Program.cs
+++ TimeMachine/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MinimalHelpers.OpenApi;
 using TimeMachine.DataAccessLayer;
 using TimeMachine.Models;
 
@@ -9,7 +10,10 @@ builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetCon
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddMissingSchemas();
+});
 
 var app = builder.Build();
 
